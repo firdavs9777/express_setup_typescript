90ae2f55f1338d3f9a9120cd1d69550f044a613e
firdavs9777 <fmutalipov97@gmail.com>
2024-04-09 09:31:34 +0900

    New methods added for api testing

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -9,6 +9,32 @@ export const createTodo: RequestHandler = (req, res, next) => {
   const newTodo = new Todo(Math.random().toString(), text);
 
   TODOS.push(newTodo);
-
   res.status(201).json({message: 'Created the todo.', createdTodo: newTodo});
 };
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ message:'Showing the list of todo files', todos: TODOS})
+}
+
+export const updateTodo: RequestHandler<{id: string}> = (req, res, next) => {
+  const todoId = req.params.id;
+  const updatedText = (req.body as { text: string }).text;
+  const todoIndex = TODOS.findIndex(todo => todoId === todoId);
+  if (todoIndex < 0)
+  {
+    throw new Error('Could not find todo item');
+  }
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+  res.json({ message: 'Updated', updateTodo: TODOS[todoIndex] });
+}
+export const deleteToDo: RequestHandler = (req, res, next) => {
+  const todoId = req.params.todoId;
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+  if (todoIndex === -1)
+  {
+    return res.status(404).json({ message: 'Todo not found' }); 
+  }
+  TODOS.splice(todoIndex, 1);
+  res.status(200).json({ mesage: 'Todo deleted' });
+}
