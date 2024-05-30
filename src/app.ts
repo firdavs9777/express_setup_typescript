fcbda006966d7b6ae9ba6e918a18a017f1007d28
firdavs9777 <fmutalipov97@gmail.com>
2024-05-30 15:24:32 +0900

    single product api added

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,31 @@
 import express, { Request, Response, NextFunction } from 'express';
-// import
 import todoRoutes from './routes/todos'
 import { json } from 'body-parser';
 import dotenv from 'dotenv';
-import { products } from '../data/products';
 
-const port = 5001;
+import { products, ProductType } from '../data/products';
+const app = express(); 
+const cors = require('cors');
+// Dotenv implemented and imported
 dotenv.config();
-
-console.log("DB_HOST:", process.env.DB_HOST);
-
-console.log('test')
-
-const app = express();
+const port:number = parseInt(process.env.PORT || '5002', 10);
 app.use(json());
 app.use('/todos', todoRoutes);
+app.use(cors);
+
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
 })
 app.get('/', (req: Request, res: Response) => {
+  console.log(req);
   res.send('Api is running')
 })
 app.get('/api/v1/products', (req: Request, res: Response) => {
   res.json(products)
+})
+app.get('/api/v1/products/:id', (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const product = products.find((p:ProductType ) => p.id === id);
+  res.json(product)
 })
 app.listen(port, ()=> console.log(`Server is running on port ${port}`));
