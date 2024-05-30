87d98b0000afb95af350ab4d11eb36e8446ae390
firdavs9777 <fmutalipov97@gmail.com>
2024-05-30 13:23:04 +0900

    sample products added

--- dist/src/app.js
+++ dist/src/app.js
@@ -8,6 +8,7 @@ const express_1 = __importDefault(require("express"));
 const todos_1 = __importDefault(require("./routes/todos"));
 const body_parser_1 = require("body-parser");
 const dotenv_1 = __importDefault(require("dotenv"));
+const products_1 = require("../data/products");
 const port = 5001;
 dotenv_1.default.config();
 console.log("DB_HOST:", process.env.DB_HOST);
@@ -18,7 +19,7 @@ app.use('/todos', todos_1.default);
 app.use((err, req, res, next) => {
     res.status(500).json({ message: err.message });
 });
-app.get('/', (req, res) => {
-    res.send('Api is running');
+app.get('/api/v1/products', (req, res) => {
+    res.json(products_1.products);
 });
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,15 @@ import express, { Request, Response, NextFunction } from 'express';
 // import
 import todoRoutes from './routes/todos'
 import { json } from 'body-parser';
-const port = 5001;
+import dotenv from 'dotenv';
+import { products } from '../data/products';
 
+const port = 5001;
+dotenv.config();
+
+console.log("DB_HOST:", process.env.DB_HOST);
+
+console.log('test')
 
 const app = express();
 app.use(json());
@@ -11,7 +18,7 @@ app.use('/todos', todoRoutes);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
 })
-app.get('/', (req: Request, res: Response) => {
-  res.send('Api is running')
+app.get('/api/v1/products', (req: Request, res: Response) => {
+  res.json(products)
 })
 app.listen(port, ()=> console.log(`Server is running on port ${port}`));
